522cb7c2f122e067757373c9398afb81bfa8aac7
Cocell <147223542+Cocell0@users.noreply.github.com>
2023-12-27 18:40:28 +0600

    Eyo

--- posts-script.js
+++ posts-script.js
@@ -1,37 +1,30 @@
 function updateTimeAgo() {
     var datePosts = document.querySelectorAll(".datePost");
-  
+
     datePosts.forEach(function (datePost) {
-      var span = datePost.querySelector("span[data-timestamp]");
-      var dateString = span ? span.dataset.timestamp : null;
+      var dateString = datePost.dataset.timestamp;
       var postDate = dateString ? new Date(dateString) : new Date();
       var now = new Date();
       var timeDiff = now - postDate;
-  
+
       var seconds = Math.floor(timeDiff / 1000);
       var minutes = Math.floor(seconds / 60);
       var hours = Math.floor(minutes / 60);
       var days = Math.floor(hours / 24);
-      var months = Math.floor(days / 30);
-      var years = Math.floor(days / 365);
-  
+
       var displayText =
-        years > 0
-          ? years + (years === 1 ? " year" : " years") + " ago"
-          : months > 0
-          ? months + (months === 1 ? " month" : " months") + " ago"
-          : days > 0
-          ? days + (days === 1 ? " day" : " days") + " ago"
+        days > 0
+          ? days + " days ago"
           : hours > 0
-          ? hours + (hours === 1 ? " hour" : " hours") + " ago"
+          ? hours + " hours ago"
           : minutes > 0
-          ? minutes + (minutes === 1 ? " minute" : " minutes") + " ago"
-          : seconds + (seconds === 1 ? " second" : " seconds") + " ago";
-  
-      datePost.textContent = "Posted: " + displayText;
+          ? minutes + " minutes ago"
+          : seconds + " seconds ago";
+
+      datePost.textContent = displayText;
     });
   }
-  
+
   updateTimeAgo();
-  
+
   setInterval(updateTimeAgo, 60000);
